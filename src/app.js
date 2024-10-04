77d093baf8669cca95bada89b32322dfd29f2d00
Surya Ganesh <vissapragadaganesh1@gmail.com>
2024-10-04 12:06:36 +0530

    routers/controlles

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
+
 export{app}
