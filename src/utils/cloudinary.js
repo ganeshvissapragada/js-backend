ebb2643ba74108b1162eba97d08e46d9c23c60cd
Surya Ganesh <vissapragadaganesh1@gmail.com>
2024-10-04 10:41:29 +0530

    file upload utilities

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from"fs" //node filesystem
+
+
+
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary= async(localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+     const response=  await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("flie is uploaded on cloudinary",response.url);
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)//removes locally saved temp file from sever
+        return null;
+    }
+}
+
+
+
+export{uploadOnCloudinary}
+
+
